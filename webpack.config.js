ee8f49500b7f7ba9e3f835f3d22a45dfb82438d6
Natt Piyapramote <nattster@gmail.com>
2015-11-29 15:23:06 +0700

    template for react + webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html",
+  },
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+    ],
+  }
+}
